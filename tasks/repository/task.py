5d4bd148a565f332eafc404edf0343f9ed72057b
Zhukov Daniil <dmzhukov@outlook.com>
2021-10-04 12:28:14 +0300

    Fix find_tasks join and filter bug

--- tasks/repository/task.py
+++ tasks/repository/task.py
@@ -18,11 +18,11 @@ class TaskRepository:
     def find_tasks(self, user_id: Optional[UUID] = None, course_id: Optional[UUID] = None,
                    limit: int = 10, offset: int = 0):
         query = self._session.query(Task)
+        if course_id:
+            query = query.filter_by(course_id=course_id)
         if user_id:
             query = query.join(Assignment, isouter=True).filter(or_(Task.author_id == user_id,
                                                                     Assignment.assignee_id == user_id))
-        if course_id:
-            query = query.filter_by(course_id=course_id)
         return query.offset(offset).limit(limit).all(), query.order_by(None).count()
 
 
